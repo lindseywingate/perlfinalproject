242a479a80879ca7b38f08796f6e31654c2a95b5
lindsey wingate <lindseymwingate@gmail.com>
2017-02-14 10:54:19 -0600

    purged old files. Starting 457 project

--- public_html/457/ListTitles.php
+++ public_html/457/ListTitles.php
@@ -2,55 +2,44 @@
 <?php
  
 if ( $_POST['act'] == "List the titles" ) {
-  /************************************************************
+	$username = "lindseywingate";
+	$password = "password";
+	$database = "lindseywingate";
+	$host		 = "mysqldev.aero.und.edu";
+ 
+	// Connect to the database.
+	$conn = mysql_connect( $host, $username, $password );
+ 
+	// Select a database.
+	mysql_select_db( $database, $conn );
 
-  This program shows how to list the book titles from the
-    books table.
-  To use this program, you need to create a table
-    books by using the following command:
-  mysql> create table books (
-      ->   title   char(128),
-      ->   ISBN    char(20) primary key,
-      ->   price   real );
-  Query OK, 0 rows affected (0.07 sec)
+	$title = $_POST['title']; 
+	// Compose the query.
+	$query = "select * from books where title like '$title'";
  
-  *************************************************************/
-  $username = "lwingate";
-  $password = "password";
-  $database = "school";
-  $host     = "mysqldev.aero.und.edu";
+	// Execute the query.
+	$result = mysql_query( $query );
  
-  // Connect to the database.
-  $conn = mysql_connect( $host, $username, $password );
+	// Find the row number of the results.
+	$num = mysql_numrows( $result );
  
-  // Select a database.
-  mysql_select_db( $database, $conn );
- 
-  // Compose the query.
-  $query  = "select title from books where ";
-  $query  = $query . "title like '%$_POST[title]%'";
- 
-  // Execute the query.
-  $result = mysql_query( $query );
- 
-  // Find the row number of the results.
-  $num    = mysql_numrows( $result );
- 
-  // Print the results row by row.
-  $i = 0;
-  while ( $i < $num ) {
-    $title = mysql_result( $result, $i, "title" );
-    echo  "$title";
-    $i++;
-  }
-  // Close the database.
-  mysql_close( );
+	// Print the results row by row.
+	$i = 0;
+	while ( $i < $num ) {
+		$title = mysql_result( $result, $i, "title" );
+		$ISBN = mysql_result( $result, $i, "ISBN" );
+		$cost = mysql_result ( $result, $i, "price" );
+		echo	"$title, $ISBN, $cost";
+		$i++;
+	}
+	// Close the database.
+	mysql_close( );
 }
 elseif ( $_POST["act"] == "Help" ) {
-  echo  "No help at this moment.";
+	echo "No help at this moment.";
 }
 else {
-  echo  "No such option: $_POST['act']";
+	echo "No such option.";
 }
 ?>
 
