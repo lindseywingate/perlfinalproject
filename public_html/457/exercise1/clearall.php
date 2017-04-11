7eeb10d02325f8f1a4b7bba97a70829f9b4ce305
lindsey wingate <lindseymwingate@gmail.com>
2017-04-11 14:08:11 -0500

    adding missed files

---
+++ public_html/457/exercise1/clearall.php
@@ -0,0 +1,25 @@
+<?php
+#this page clears all the data in the database system so Dr. Hu can test my system
+
+if($_POST['clearall'] = "clearall" ) {
+    $username = "lindseywingate";
+    $password = "password";
+    $database = "lindseywingate";
+    $host        = "mysqldev.aero.und.edu";
+ 
+    $conn = mysql_connect( $host, $username, $password );
+    mysql_select_db( $database, $conn );
+
+	$books = "truncate books";
+	$purchases = "truncate purchases";
+	$shoppingcart = "truncate shoppingcart";
+
+	mysql_query($books);
+	mysql_query($purchases);
+	mysql_query($shoppingcart);
+
+	header("Location: http://people.aero.und.edu/~lwingate/457/exercise1/login.php");
+}
+
+
+?>
