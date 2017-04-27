10a35cca820d9144d90dbdbabecc62b571790d66
lindsey wingate <lindseymwingate@gmail.com>
2017-04-27 10:56:11 -0500

    more stuff

--- public_html/457/2/login_get.php
+++ public_html/457/2/login_get.php
@@ -1,20 +1,36 @@
 <?php
-  $username = $_GET['username'];
-  $password = $_GET['password'];
-  $host     = "mysqldev.aero.und.edu";
-  $conn     = mysql_connect( $host, "lindseywingate", "password" );
+	$username = $_GET['username'];
+	$password = $_GET['password'];
+	$database = 'lindseywingate';
+	$host = "mysqldev.aero.und.edu";
+	$conn = mysql_connect ($host, "lindseywingate", "password");
  
-  if ( !$conn )
-    die( 'Could not connect to MySQL: ' . mysql_error( ) );
-  else {
-    mysql_select_db( "database", $conn );
-    $sql  = "SELECT type_user FROM users WHERE ";
-    $sql .= "name='$username' and password='$password'";
-    if ( $result = mysql_query( $sql, $conn ) ) {
-      $row  = mysql_fetch_array( $result );
-      $data = $row[0];
-    }
-    if ( $data )  echo $data;
-    mysql_close( $conn );
-  }
+	if (!$conn )
+		die('Could not connect to MySQL: ' . mysql_error());
+	else {
+		mysql_select_db($database, $conn);
+		
+		$result = mysql_query("SELECT * FROM users WHERE username='$username' and password='$password'");
+		$num = mysql_numrows($result);
+
+//		echo "THIS IS THE NUMBER OF ROWS . $num";
+		if ($num) {
+//			echo 'there were results';
+			$row = mysql_fetch_array($result);
+			if ($row['type_user']=="C") {
+//				echo 'true';	
+				$returnme = true;
+				return $returnme;
+			}
+			else {
+//				echo 'not true';
+				$returnme = false;
+				return $returnme;
+			}
+		}
+//		else {
+//			echo 'there were no results';
+//		}
+		mysql_close($conn);
+	}
 
